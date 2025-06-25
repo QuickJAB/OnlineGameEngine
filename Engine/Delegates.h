79f5de520bebd66d75f6c6667ae9eccb9eabb890
QuickJAB <Redman9782@gmail.com>
2025-06-25 23:50:34 +0100

    Added Multicast delegate support
    Added a new MulticastDelegate template so allow for multiple void returned methods to be bound to a single delegate.
    
    Also improved and optimized the regular delegate template.

--- Engine/Delegates.h
+++ Engine/Delegates.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <functional>
 
-// Generlised template declaration
+// Generalised template declaration
 template<typename T>
 class Delegate;
 
@@ -10,16 +10,23 @@ template<typename Ret, typename... Args>
 class Delegate<Ret(Args...)>
 {
 private:
-	// The function this template is storing and calling, initalized as nullptr to ensure isBound returns false initally
+	// The function this delegate is storing and calling, initialized as nullptr to ensure isBound returns false initally
 	std::function<Ret(Args...)> m_func = nullptr;
 
 public:
 	template<typename Owner>
 	// Bind for setting m_func as a member function
 	// in_owner		A pointer to the instance of the class that owns the given method (in_method)
-	// in_method	A reference to the method you wish to set m_func too
+	// in_method	A pointer to the method you wish to set m_func too
 	void bind(Owner* in_owner, Ret(Owner::* in_method)(Args...))
 	{
+		// Early return and force unbind if the given owner is invalid
+		if (in_owner == nullptr)
+		{
+			unbind();
+			return;
+		}
+
 		// Lambda that returns owner->method(args);
 		m_func = [in_owner, in_method](Args... args)->Ret {
 			// (in_owner->*in_method) represents in_owner->method
@@ -31,9 +38,16 @@ public:
 	template<typename Owner>
 	// Bind for setting m_func as a const member function
 	// in_owner		A const pointer to the instance of the class that owns the given method (in_method)
-	// in_method	A reference to the const method you wish to set m_func too
+	// in_method	A pointer to the const method you wish to set m_func too
 	void bind(const Owner* in_owner, Ret(Owner::* in_method)(Args...) const)
 	{
+		// Early return and force unbind if the given owner is invalid
+		if (in_owner == nullptr)
+		{
+			unbind();
+			return;
+		}
+
 		// Lambda that returns owner->method(args);
 		m_func = [in_owner, in_method](Args... args)->Ret {
 			// (in_owner->*in_method) represents in_owner->method
@@ -51,18 +65,123 @@ public:
 	// Calls m_func if it's bound
 	Ret broadcast(Args... in_args) const
 	{
-		// Check if Ret is void
-		if constexpr(std::is_void_v<Ret>)
+		// Return the default constructed Ret if the bound function is invalid
+		if (!isBound())
 		{
-			// (in_owner->*in_method) represents in_owner->method
-			// std::forward is used to retain the lvalue and rvalue states of the given arguments when passing it into the function
-			if (isBound()) m_func(std::forward<Args>(in_args)...);
+			// If the return type is not void then return the default constructed value of Ret
+			if constexpr (!std::is_void_v<Ret>) return Ret{};
+
+			// If the return type is void then just early return from broadcast
+			return;
 		}
-		else
+
+		// Execute the bound function
+		return m_func(std::forward<Args>(in_args)...);
+	}
+};
+
+
+
+
+
+// Generalised template declaration
+template<typename T>
+class MulticastDelegate;
+
+// Specialised template definition
+template<typename... Args>
+class MulticastDelegate<void(Args...)>
+{
+private:
+	// A struct to link the owning instance to the function for future removal logic
+	struct OwnerFunctionPair
+	{
+		// void* is used because the array of this struct cannot be tempalted
+		void* owner = nullptr;
+		std::function<void(Args...)> func;
+
+		OwnerFunctionPair(void* in_owner, std::function<void(Args...)> in_func)
 		{
-			// If Ret is a non-void type then attempt to call m_func if it's bound, otherwise return a default constructed value of Ret
+			owner = in_owner;
+			func = in_func;
+		}
+	};
+
+	// The array of owner-function pairs this delegate is storing and calling
+	std::vector<OwnerFunctionPair> m_functions;
+
+	// Clear all the bound functions related to a specific object from the m_functions vector
+	void unbindAllFromOwner(void* in_owner)
+	{
+		// Early return if there are no bound functions or the passed object is invalid
+		if (m_functions.empty() || in_owner == nullptr) return;
+
+		// Use the stl remove_if to pass a lambda to erase so we can remove the pairs in-place
+		m_functions.erase(std::remove_if(m_functions.begin(), m_functions.end(),
+			[in_owner](const OwnerFunctionPair& pair) {
+				return pair.owner == in_owner;
+			}),
+			m_functions.end()
+		);
+	}
+
+public:
+	template<typename Owner>
+	// Add a method to the vector of stored functions
+	// in_owner		A pointer to the instance of the class that owns the given method (in_method)
+	// in_method	A reference to the method you wish to set m_func too
+	void add(Owner* in_owner, void(Owner::* in_method)(Args...))
+	{
+		// Early return if in invalid object was passed
+		if (in_owner == nullptr) return;
+
+		// Lambda that returns owner->method(args);
+		std::function<void(Args...)> func = [in_owner, in_method](Args... args) {
+				// (in_owner->*in_method) represents in_owner->method
+				// std::forward is used to retain the lvalue and rvalue states of the given arguments when passing it into the function
+				return (in_owner->*in_method)(std::forward<Args>(args)...);
+			};
+
+		m_functions.push_back(OwnerFunctionPair(static_cast<void*>(in_owner), std::move(func)));
+	}
+
+	template<typename Owner>
+	// Add a const method to the vector of stored functions
+	// in_owner		A const pointer to the instance of the class that owns the given method (in_method)
+	// in_method	A reference to the const method you wish to set m_func too
+	void add(const Owner* in_owner, void(Owner::* in_method)(Args...) const)
+	{
+		// Early return if in invalid object was passed
+		if (in_owner == nullptr) return;
+
+		// Lambda that returns owner->method(args);
+		std::function<void(Args...)> func = [in_owner, in_method](Args... args) {
+				// (in_owner->*in_method) represents in_owner->method
+				// std::forward is used to retain the lvalue and rvalue states of the given arguments when passing it into the function
+				return (in_owner->*in_method)(std::forward<Args>(args)...);
+			};
+
+		m_functions.push_back(OwnerFunctionPair(static_cast<void*>(in_owner), std::move(func)));
+	}
+
+	// QoL publically exposed function so the user doesn't have to cast the object to void*
+	template<typename Owner>
+	void unbindAllFromOwner(Owner* in_owner)
+	{
+		unbindAllFromOwner(static_cast<void*>(in_owner));
+	}
+
+	// Calls m_func if it's bound
+	void broadcast(Args... in_args) const
+	{
+		// Early return if there are no bound functions
+		if (m_functions.empty()) return;
+
+		for (const OwnerFunctionPair& pair : m_functions)
+		{
+			// Execute the function
 			// std::forward is used to retain the lvalue and rvalue states of the given arguments when passing it into the function
-			return isBound() ? m_func(std::forward<Args>(in_args)...) : Ret{};
+			pair.func(std::forward<Args>(in_args)...);
 		}
 	}
 };
