bd06fd4e71546f07f257037e771248bd98a120bf
QuickJAB <Redman9782@gmail.com>
2025-06-29 19:31:06 +0100

    Cleaned up the premake file

--- premake5.lua
+++ premake5.lua
@@ -4,193 +4,72 @@ workspace "OnlineGame"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-IncludeDir = {}
-IncludeDir["SDL"] = "extern/SDL/install/include"
-IncludeDir["ENet"] = "extern/enet/install/include"
-IncludeDir["stb"] = "extern/stb"
+IncludeDir = {
+    SDL = "extern/SDL/install/include",
+    ENet = "extern/enet/install/include",
+    stb  = "extern/stb"
+}
 
-LibraryDir = {}
-LibraryDir["SDL_Debug"] = "extern/SDL/install/lib"
-LibraryDir["SDL_Release"] = "extern/SDL/install/lib"
-LibraryDir["ENet_Debug"] = "extern/enet/install/lib"
-LibraryDir["ENet_Release"] = "extern/enet/install/lib"
-
-DLLDir = "extern/SDL/install/bin"
+local function copySDLdll(targetDir)
+    postbuildcommands {
+        'cmd /c if exist "..\\extern\\SDL\\install\\bin\\SDL3.dll" copy /Y "..\\extern\\SDL\\install\\bin\\SDL3.dll" "%{cfg.targetdir}"'
+    }
+end
 
 project "Engine"
     location "Engine"
     kind "StaticLib"
     language "C++"
     cppdialect "C++23"
-    staticruntime "off"            -- Use dynamic CRT (/MD)
-
+    staticruntime "on"
+    systemversion "latest"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files {
-        "Engine/**.h",
-        "Engine/**.cpp"
-    }
-
-    includedirs {
-        "Engine",
-        "%{IncludeDir.SDL}",
-        "%{IncludeDir.ENet}",
-        "%{IncludeDir.stb}"
-    }
-
-    filter "configurations:Debug"
-        runtime "Debug"
-        libdirs {
-            "%{LibraryDir.SDL_Debug}",
-            "%{LibraryDir.ENet_Debug}"
-        }
-        links {
-            "SDL3", "enet", "ws2_32", "winmm"
-        }
-
-    filter "configurations:Release"
-        runtime "Release"
-        libdirs {
-            "%{LibraryDir.SDL_Release}",
-            "%{LibraryDir.ENet_Release}"
-        }
-        links {
-            "SDL3", "enet", "ws2_32", "winmm"
-        }
-
-    filter {}
+    files { "%{prj.location}/**.h", "%{prj.location}/**.cpp" }
+    includedirs { "%{IncludeDir.SDL}", "%{IncludeDir.ENet}", "%{IncludeDir.stb}" }
+    libdirs { "extern/SDL/install/lib", "extern/enet/install/lib" }
+    links { "SDL3", "enet", "ws2_32", "winmm" }
 
 project "Game"
     location "Game"
     kind "StaticLib"
     language "C++"
     cppdialect "C++23"
-    staticruntime "off"            -- Use dynamic CRT (/MD)
-
+    staticruntime "on"
+    systemversion "latest"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    files { "%{prj.location}/**.h", "%{prj.location}/**.cpp" }
+    includedirs { "Engine", "%{IncludeDir.SDL}", "%{IncludeDir.ENet}", "%{IncludeDir.stb}" }
+    links { "Engine" }
+    dependson { "Engine" }
 
-    files {
-        "Game/**.h",
-        "Game/**.cpp"
-    }
-	
-	    includedirs {
-        "Engine",
-        "%{IncludeDir.SDL}",
-        "%{IncludeDir.ENet}",
-        "%{IncludeDir.stb}"
-    }
-
-    links {
-        "Engine"
-    }
-
-    dependson {
-        "Engine"
-    }
-	
-	    filter "configurations:Debug"
-        runtime "Debug"
-        postbuildcommands {
-            'cmd /c if exist "..\\extern\\SDL\\install\\bin\\SDL3.dll" copy /Y "..\\extern\\SDL\\install\\bin\\SDL3.dll" "%{cfg.targetdir}"'
-        }
-
-    filter "configurations:Release"
-        runtime "Release"
-        postbuildcommands {
-            'cmd /c if exist "..\\extern\\SDL\\install\\bin\\SDL3.dll" copy /Y "..\\extern\\SDL\\install\\bin\\SDL3.dll" "%{cfg.targetdir}"'
-        }
-
-    filter {}
-	
 project "Client"
     location "Client"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++23"
-    staticruntime "off"            -- Use dynamic CRT (/MD)
-
+    staticruntime "on"
+    systemversion "latest"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files {
-        "Client/**.h",
-        "Client/**.cpp"
-    }
-
-    includedirs {
-        "Engine",
-        "%{IncludeDir.SDL}",
-        "%{IncludeDir.ENet}",
-        "%{IncludeDir.stb}",
-		"Game",
-    }
-
-    links {
-        "Game"
-    }
-
-    dependson {
-        "Game"
-    }
-
-    filter "configurations:Debug"
-        runtime "Debug"
-        postbuildcommands {
-            'cmd /c if exist "..\\extern\\SDL\\install\\bin\\SDL3.dll" copy /Y "..\\extern\\SDL\\install\\bin\\SDL3.dll" "%{cfg.targetdir}"'
-        }
-
-    filter "configurations:Release"
-        runtime "Release"
-        postbuildcommands {
-            'cmd /c if exist "..\\extern\\SDL\\install\\bin\\SDL3.dll" copy /Y "..\\extern\\SDL\\install\\bin\\SDL3.dll" "%{cfg.targetdir}"'
-        }
-
-    filter {}
+    files { "%{prj.location}/**.h", "%{prj.location}/**.cpp" }
+    includedirs { "Engine", "%{IncludeDir.SDL}", "%{IncludeDir.ENet}", "%{IncludeDir.stb}", "Game" }
+    links { "Game" }
+    dependson { "Game" }
+    postbuildcommands { copySDLdll("%{cfg.targetdir}") }
 
 project "Server"
     location "Server"
     kind "ConsoleApp"
     language "C++"
     cppdialect "C++23"
-    staticruntime "off"            -- Use dynamic CRT (/MD)
-
+    staticruntime "on"
+    systemversion "latest"
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-    files {
-        "Server/**.h",
-        "Server/**.cpp"
-    }
-
-    includedirs {
-        "Engine",
-        "%{IncludeDir.SDL}",
-        "%{IncludeDir.ENet}",
-        "%{IncludeDir.stb}",
-		"Game",
-    }
-
-    links {
-        "Game"
-    }
-
-    dependson {
-        "Game"
-    }
-
-    filter "configurations:Debug"
-        runtime "Debug"
-        postbuildcommands {
-            'cmd /c if exist "..\\extern\\SDL\\install\\bin\\SDL3.dll" copy /Y "..\\extern\\SDL\\install\\bin\\SDL3.dll" "%{cfg.targetdir}"'
-        }
-
-    filter "configurations:Release"
-        runtime "Release"
-        postbuildcommands {
-            'cmd /c if exist "..\\extern\\SDL\\install\\bin\\SDL3.dll" copy /Y "..\\extern\\SDL\\install\\bin\\SDL3.dll" "%{cfg.targetdir}"'
-        }
-
-    filter {}
+    files { "%{prj.location}/**.h", "%{prj.location}/**.cpp" }
+    includedirs { "Engine", "%{IncludeDir.SDL}", "%{IncludeDir.ENet}", "%{IncludeDir.stb}", "Game" }
+    links { "Game" }
+    dependson { "Game" }
+    postbuildcommands { copySDLdll("%{cfg.targetdir}") }
