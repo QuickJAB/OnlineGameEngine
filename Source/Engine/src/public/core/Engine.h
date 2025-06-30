4dc064a66e988fecc09fedf805826201ea5fb1c3
QuickJAB <Redman9782@gmail.com>
2025-06-30 22:01:10 +0100

    Cleared the engine class

--- Source/Engine/src/public/core/Engine.h
+++ Source/Engine/src/public/core/Engine.h
@@ -1,26 +1,8 @@
 #pragma once
-
-#include <atomic>
-
-class GameInstance;
-
 class Engine
 {
 public:
-	Engine(std::atomic<bool>& in_running, float in_fixedTimeStep) : 
-		m_running(in_running), m_fixedTimeStep(in_fixedTimeStep) {}
+	Engine() = default;
 	~Engine() = default;
-
-	bool init(GameInstance* in_gInst);
-	void run();
-
-private:
-	// General
-	std::atomic<bool>& m_running;
-	float m_fixedTimeStep = 0.f;
-
-	GameInstance* m_gInst = nullptr;
-
-	void cleanup();
 };
 
