4f3416780ffbff073dd145383679d279852d6289
QuickJAB <Redman9782@gmail.com>
2025-07-01 23:22:08 +0100

    Added component containers
    Added the component container
    Added a function to add a component to an entity

--- Source/Engine/src/public/ECS/ECS.h
+++ Source/Engine/src/public/ECS/ECS.h
@@ -3,6 +3,20 @@
 #include <cstdint>
 #include <queue>
 #include <vector>
+#include <unordered_map>
+
+#include <print>
+
+struct IComponentContainer
+{
+	virtual ~IComponentContainer() {}
+};
+
+template <typename T>
+struct ComponentContainer : public IComponentContainer
+{
+	std::vector<T> components;
+};
 
 class ECS
 {
@@ -10,23 +24,43 @@ public:
 	ECS() = default;
 	~ECS() = default;
 
-	// Create an entity with no components
+	// Create an entity
 	const uint32_t createEntity();
-
-	// Create an entity with specified components
-	template<typename... Components>
-	const uint32_t createEntity(Components... in_components);
 
 	// Destroy an entity and all its components
 	void destroyEntity(const uint32_t& in_entity);
+
+	template <typename T>
+	// Add a component to an entity
+	void addComponent(const uint32_t& in_entity, const std::string& in_componentType, const T& in_component)
+	{
+		// Check if a container exists for this component type
+		if (!m_componentContainers.contains(in_componentType))
+		{
+			// If it doesn't, create a new ComponentContainer of the given component with an id of the given component type
+			m_componentContainers.insert(std::pair<std::string, IComponentContainer*>(in_componentType, new ComponentContainer<T>()));
+		}
+
+		// Get a pointer to a generic container
+		IComponentContainer* genericContainer = m_componentContainers[in_componentType];
+
+		// Cast it to a pointer to a container of the given type
+		ComponentContainer<T>* componentContainer = dynamic_cast<ComponentContainer<T>*>(genericContainer);
+		
+		// Add the given component to the containers vector
+		componentContainer->components.push_back(in_component);
+	}
 
 private:
 	// The id to be given to the next entity that is created
 	uint32_t m_nextEntityId = 0;
 
 	// A store of ids that can be reused as their entity has been destroyed
-	std::queue<const uint32_t> m_freedEntityIds;
+	std::queue<uint32_t> m_freedEntityIds;
 
 	// A store of all current entities
 	std::vector<uint32_t> m_entities;
+
+	// A map with the component type as the key and a pointer to its container as the value
+	std::unordered_map<std::string, IComponentContainer*> m_componentContainers;
 };
