8115d6b5e9f3632f86648e6bdb7152f1ca2b10cd
QuickJAB <Redman9782@gmail.com>
2025-07-02 21:50:24 +0100

    Abstracted and simplified the ECS
    Moved add/remove/get functionality to the component container struct
    
    Removed unnecessary comments
    Clarified unclear comments

--- Source/Engine/src/public/ECS/ECS.h
+++ Source/Engine/src/public/ECS/ECS.h
@@ -16,8 +16,48 @@ struct ComponentContainer : public IComponentContainer
 	// Stores the components in densly packed memory
 	std::vector<T> dense;
 
-	// Stores the memory offset of a component in dense at the index of an entity id
+	// Stores the memory offset of a component in dense at the index of the owning entity
 	std::vector<size_t> sparse;
+
+	template <typename T>
+	void add(const uint32_t in_entity)
+	{
+		dense.push_back(T());
+
+		// Set the index at entity in sparse to the memory offset of the component in dense
+		sparse.insert(sparse.begin() + in_entity, dense.size() - 1);
+	}
+
+	template <typename T>
+	void remove(const uint32_t in_entity)
+	{
+		if (dense.empty()) return;
+
+		// Get a pointer to the entity in sparse for the component at the back of dense
+		auto it = std::find(sparse.begin(), sparse.end(), dense.size() - 1);
+
+		// Swap the component at the back of dense with with one that's being removed
+		iter_swap(dense.begin() + sparse[in_entity], dense.end() - 1);
+
+		dense.pop_back();
+
+		// Update the swapped entities memory offset in sparse to the new value
+		sparse[it - sparse.begin()] = sparse[in_entity];
+
+		sparse[in_entity] = (size_t)-1;
+	}
+
+	template <typename T>
+	T* get(const uint32_t in_entity)
+	{
+		if (dense.empty()) return nullptr;
+
+		size_t memOffset = sparse[in_entity];
+
+		if (memOffset > dense.size() - 1) return nullptr;
+
+		return &dense[memOffset];
+	}
 };
 
 class ECS
@@ -26,14 +66,10 @@ public:
 	ECS() = default;
 	~ECS() = default;
 
-	// Create an entity
 	const uint32_t createEntity();
-
-	// Destroy an entity and all its components
 	void destroyEntity(const uint32_t in_entity);
 
 	template <typename T>
-	// Add a component to an entity
 	void addComponent(const uint32_t in_entity)
 	{
 		// Try and get a pointer to the container of the given component
@@ -42,84 +78,43 @@ public:
 		// Check if a container exists for this component type
 		if (componentContainer == nullptr)
 		{
-			// If it doesn't, create a new ComponentContainer of the given component with an id of the given component type
-			m_componentContainers.insert(std::pair<std::string, IComponentContainer*>(typeid(T).name(), new ComponentContainer<T>()));
-
-			// Get a pointer to the newly created container
-			componentContainer = tryGetComponentContainer<T>();
+			componentContainer = createComponentContainer<T>();
 
 			// Set a default size of the sparse array to be 100 (THIS IS W.I.P)
 			componentContainer->sparse.resize(100, (size_t)-1);
 		}
 
-		// Add the given component to the dense vector
-		componentContainer->dense.push_back(T());
-
-		// Add the memory offset of the component in dense vector to the index of the entity in sparse
-		componentContainer->sparse.insert(componentContainer->sparse.begin() + in_entity, componentContainer->dense.size() - 1);
+		componentContainer->add<T>(in_entity);
 	}
 
 	template <typename T>
-	// Remove a component from an entity
 	void removeComponent(const uint32_t in_entity)
 	{
-		// Get the container for the given component, return if non exist
 		ComponentContainer<T>* componentContainer = tryGetComponentContainer<T>();
 		if (componentContainer == nullptr) return;
 
-		// Early return if the component container is empty
-		if (componentContainer->dense.empty()) return;
-
-		// Get a pointer to the entity id in sparse of the component at the back of dense
-		auto it = std::find(componentContainer->sparse.begin(), componentContainer->sparse.end(), componentContainer->dense.size() - 1);
-
-		// Cache the entity that is being moved from the back
-		uint32_t otherEntity = it - componentContainer->sparse.begin();
-
-		// Swap the component that is being removed with the back of the dense vector
-		iter_swap(componentContainer->dense.begin() + componentContainer->sparse[in_entity], componentContainer->dense.end() - 1);
-
-		// Remove the last component from the dense vector
-		componentContainer->dense.pop_back();
-
-		// Set the found entities memory offset value in sparse to the offset value of the component that is being removed
-		componentContainer->sparse[otherEntity] = componentContainer->sparse[in_entity];
-
-		// Set the memory offset value of the entity who's component is being removed to 1 above the max entity id
-		componentContainer->sparse[in_entity] = (size_t)-1;
+		componentContainer->remove<T>(in_entity);
 	}
 
 	template <typename T>
 	T* getComponent(const uint32_t in_entity)
 	{
-		// Get the container for the given component, return if non exist
 		ComponentContainer<T>* componentContainer = tryGetComponentContainer<T>();
 		if (componentContainer == nullptr) return nullptr;
 
-		// Return nullptr if the component container is empty
-		if (componentContainer->dense.empty()) return nullptr;
-
-		// Get the memory offset of the entities component in dense
-		size_t memOffset = componentContainer->sparse[in_entity];
-
-		// Return nullptr if this entity doesn't have a component of that type
-		if (memOffset > componentContainer->dense.size() - 1) return nullptr;
-
-		// Return a pointer to the entities component in dense
-		return &componentContainer->dense[memOffset];
+		return componentContainer->get<T>(in_entity);
 	}
 
 private:
-	// The id to be given to the next entity that is created
+	// The id to be given to the next created entity
 	uint32_t m_nextEntityId = 0;
 
-	// A store of ids that can be reused as their entity has been destroyed
+	// Previously used entity ids ready for reassignment
 	std::queue<uint32_t> m_freedEntityIds;
 
-	// A store of all current entities
 	std::vector<uint32_t> m_entities;
 
-	// A map with the component type as the key and a pointer to its container as the value
+	// A map of component type to component container
 	std::unordered_map<std::string, IComponentContainer*> m_componentContainers;
 
 	template <typename T>
@@ -128,13 +123,23 @@ private:
 		// Get the components type as a string
 		std::string componentType = typeid(T).name();
 
-		// Early return if a container for that component doesn't exist
 		if (!m_componentContainers.contains(componentType)) return nullptr;
 
-		// Get a pointer to a generic container
-		IComponentContainer* genericContainer = m_componentContainers[componentType];
+		// Cast the pointer to the correct type and return a pointer to it
+		return static_cast<ComponentContainer<T>*>(m_componentContainers[componentType]);
+	}
 
-		// Cast it to a pointer to a container of the given type
-		return static_cast<ComponentContainer<T>*>(genericContainer);
+	template <typename T>
+	ComponentContainer<T>* createComponentContainer()
+	{
+		// Get the components type as a string
+		std::string componentType = typeid(T).name();
+
+		ComponentContainer<T>* container = new ComponentContainer<T>();
+
+		// Add the new container at key component type to the map
+		m_componentContainers.insert(std::pair<std::string, IComponentContainer*>(componentType, container));
+
+		return container;
 	}
 };
