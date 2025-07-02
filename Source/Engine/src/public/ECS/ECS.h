afe0f58bfcb55f4e66296a1d69cbd33368360268
QuickJAB <Redman9782@gmail.com>
2025-07-02 12:08:37 +0100

    Added a method to remove components

--- Source/Engine/src/public/ECS/ECS.h
+++ Source/Engine/src/public/ECS/ECS.h
@@ -5,8 +5,6 @@
 #include <vector>
 #include <unordered_map>
 
-#include <print>
-
 struct IComponentContainer
 {
 	virtual ~IComponentContainer() {}
@@ -59,6 +57,38 @@ public:
 
 		// Add the memory offset of the component in dense vector to the index of the entity in sparse
 		componentContainer->sparse.insert(componentContainer->sparse.begin() + in_entity, componentContainer->dense.size() - 1);
+	}
+
+	template<typename T>
+	// Remove a component from an entity
+	void removeComponent(const uint32_t in_entity)
+	{
+		// Get the components type as a string
+		std::string componentType = typeid(T).name();
+
+		// Early return if a container for that component doesn't exist
+		if (!m_componentContainers.contains(componentType)) return;
+
+		// Get a pointer to a generic container
+		IComponentContainer* genericContainer = m_componentContainers[componentType];
+
+		// Cast it to a pointer to a container of the given type
+		ComponentContainer<T>* componentContainer = static_cast<ComponentContainer<T>*>(genericContainer);
+
+		// Get entity id in sparse of the component at the back of dense
+		uint32_t otherEntity = *(std::find(componentContainer->sparse.begin(), componentContainer->sparse.end(), componentContainer->dense.size() - 1));
+
+		// Set the found entities memory offset value in sparse to the offset value of the component that is being removed
+		componentContainer->sparse[otherEntity] = componentContainer->sparse[in_entity];
+
+		// Set the memory offset value of the entity who's component is being removed to 1 above the max entity id
+		componentContainer->sparse[in_entity] = UINT32_MAX + 1;
+		
+		// Swap the component that is being removed with the back of the dense vector
+		iter_swap(componentContainer->dense.begin() + componentContainer->sparse[in_entity], componentContainer->dense.end() - 1);
+
+		// Remove the last component from the dense vector
+		componentContainer->dense.pop_back();
 	}
 
 private:
