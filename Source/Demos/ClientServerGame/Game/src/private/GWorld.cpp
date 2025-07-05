2948e94a8f55f815ff0fe6cd3ca7be9537074828
QuickJAB <Redman9782@gmail.com>
2025-07-05 12:17:09 +0100

    Added GWorld

--- Source/Demos/ClientServerGame/Game/src/private/GWorld.cpp
+++ Source/Demos/ClientServerGame/Game/src/private/GWorld.cpp
@@ -1,6 +1,19 @@
 #include "GWorld.h"
 
+#include <core/ECS/system/MovementSys.h>
+
 GWorld::GWorld() : World()
 {
-	
+	m_movementSys = new MovementSys(m_ecs);
+}
+
+GWorld::~GWorld()
+{
+	delete m_movementSys;
+	m_movementSys = nullptr;
+}
+
+void GWorld::update(float in_dt)
+{
+	m_movementSys->update(in_dt);
 }
--- Source/Demos/ClientServerGame/Game/src/public/GWorld.h
+++ Source/Demos/ClientServerGame/Game/src/public/GWorld.h
@@ -2,8 +2,16 @@
 
 #include <core/game base/World.h>
 
+class MovementSys;
+
 class GWorld : public World
 {
 public:
 	GWorld();
+	~GWorld();
+
+	virtual void update(float in_dt) override;
+
+private:
+	MovementSys* m_movementSys = nullptr;
 };
