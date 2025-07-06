324bfad15bdf12db908f95654d9caba1f1bb8f8d
QuickJAB <Redman9782@gmail.com>
2025-07-06 23:45:57 +0100

    Added collider comps to the walls in test level

--- Source/Demos/ClientServerGame/Game/src/private/GTestLevel.cpp
+++ Source/Demos/ClientServerGame/Game/src/private/GTestLevel.cpp
@@ -2,6 +2,7 @@
 
 #include <core/ECS/system/MovementSys.h>
 #include <core/ECS/component/TransformComp.h>
+#include <core/ECS/component/ColliderComp.h>
 
 GTestLevel::GTestLevel() : Level()
 {
@@ -29,34 +30,45 @@ void GTestLevel::load()
 	uint32_t e = m_ecs->createEntity();
 	TransformComp t = { e, 100, 100, 1720, 10 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 	e = m_ecs->createEntity();
 	t = { e, 100, 970, 1720, 10 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 	e = m_ecs->createEntity();
 	t = { e, 100, 110, 10, 860 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 	e = m_ecs->createEntity();
 	t = { e, 1810, 110, 10, 860 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 	e = m_ecs->createEntity();
 	t = { e, 260, 430, 10, 250 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 	e = m_ecs->createEntity();
 	t = { e, 1660, 430, 10, 250 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 	e = m_ecs->createEntity();
 	t = { e, 410, 555, 10, 250 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 	e = m_ecs->createEntity();
 	t = { e, 1510, 305, 10, 250 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 	e = m_ecs->createEntity();
 	t = { e, 560, 305, 10, 375 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 	e = m_ecs->createEntity();
 	t = { e, 1360, 430, 10, 375 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 	e = m_ecs->createEntity();
 	t = { e, 710, 535, 500, 10 };
 	m_ecs->addComponent<TransformComp>(e, &t);
+	m_ecs->addComponent<ColliderComp>(e);
 }
