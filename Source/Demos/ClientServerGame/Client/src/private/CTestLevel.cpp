a35cd0becc68e95d9829659d7f28915c59900db9
QuickJAB <Redman9782@gmail.com>
2025-07-06 23:12:00 +0100

    Add sprite component in client level

--- Source/Demos/ClientServerGame/Client/src/private/CTestLevel.cpp
+++ Source/Demos/ClientServerGame/Client/src/private/CTestLevel.cpp
@@ -1,8 +1,12 @@
 #include "CTestLevel.h"
 
+#include <vector>
+
 #include <core/ECS/component/TransformComp.h>
 #include <core/ECS/component/SpriteComp.h>
 #include <core/ECS/component/VelocityComp.h>
+
+using namespace std;
 
 CTestLevel::CTestLevel() : GTestLevel()
 {
@@ -25,6 +29,12 @@ void CTestLevel::update(float in_dt)
 void CTestLevel::load()
 {
 	__super::load();
+
+	const vector<uint32_t>* entities = m_ecs->getEntities();
+	for (auto it = entities->begin(); it != entities->end(); ++it)
+	{
+		m_ecs->addComponent<SpriteComp>(*it);
+	}
 }
 
 std::vector<SpriteComp>* CTestLevel::getSprites()
