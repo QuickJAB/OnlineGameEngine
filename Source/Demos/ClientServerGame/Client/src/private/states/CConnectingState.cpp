d7505089f96b84ac8defb68371244575b1d067f0
QuickJAB <Redman9782@gmail.com>
2025-07-15 22:13:49 +0100

    Swapped to ServerCommand enum usage

--- Source/Demos/ClientServerGame/Client/src/private/states/CConnectingState.cpp
+++ Source/Demos/ClientServerGame/Client/src/private/states/CConnectingState.cpp
@@ -39,14 +39,11 @@ string CConnectingState::update(float)
 
         int pktType = -1;
         const char* data = (const char*)pkt.data;
-        sscanf_s(data, "%i", &pktType);
-        switch (pktType)
+
+        if (sscanf_s(data, "%i", &pktType) > 0 && pktType == ServerCommand::startGame)
         {
-        case 1:
             startGame(data);
             return "Playing";
-        default:
-            break;
         }
     }
 
@@ -56,6 +53,7 @@ string CConnectingState::update(float)
 void CConnectingState::startGame(const char* in_data)
 {
     long long startTime = -1;
-    sscanf_s(in_data, "1t%lld", &startTime);
+    string format = to_string(ServerCommand::startGame) + "t%lld";
+    sscanf_s(in_data, format.c_str(), &startTime);
     onGameStarted.broadcast(startTime);
 }
--- Source/Demos/ClientServerGame/Server/src/private/states/SWaitingForPlayersState.cpp
+++ Source/Demos/ClientServerGame/Server/src/private/states/SWaitingForPlayersState.cpp
@@ -11,7 +11,7 @@ string SWaitingForPlayersState::update(float)
 {
     if (m_server->getNumConnections() == m_server->getMaxPlayers())
     {
-        string data = "1t" + to_string(m_server->getClockTime());
+        string data = to_string(ServerCommand::startGame) + "t" + to_string(m_server->getClockTime());
         m_server->queueOutgoingPacketData(data);
         return "Playing";
     }
