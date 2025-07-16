448d9cda8cd66d9a0a7961c48b1d3804af997134
QuickJAB <Redman9782@gmail.com>
2025-07-16 19:18:25 +0100

    Added boiler plate to GameState classes

---
+++ Source/Demos/ClientServerGame/Game/src/public/GGameState.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <network/GameState.h>
+
+class GGameState : public GameState
+{
+	GGameState() = default;
+	~GGameState() = default;
+};
---
+++ Source/Engine/src/private/network/GameState.cpp
@@ -0,0 +1,1 @@
+#include "network/GameState.h"
