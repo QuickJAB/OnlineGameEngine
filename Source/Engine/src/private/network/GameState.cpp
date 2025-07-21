5cf4964e6f2f0801e0698080e06303305126ab17
QuickJAB <Redman9782@gmail.com>
2025-07-21 22:21:30 +0100

    Gave ownership of controllers to the game state

--- Source/Engine/src/private/network/GameState.cpp
+++ Source/Engine/src/private/network/GameState.cpp
@@ -1,1 +1,18 @@
 #include "network/GameState.h"
+
+#include "core/game base/Controller.h"
+
+GameState::~GameState()
+{
+	for (auto pair : m_umControllers)
+	{
+		delete pair.second;
+	}
+
+	m_umControllers.clear();
+}
+
+Controller* const GameState::getController(const uint32_t i_cuEntityId)
+{
+	return nullptr;
+}
--- Source/Engine/src/public/network/GameState.h
+++ Source/Engine/src/public/network/GameState.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <unordered_map>
+
+class Controller;
+
 class GameState
 {
 public:
@@ -8,8 +12,12 @@ private:
 
 public:
 	GameState() = default;
-	~GameState() = default;
+	~GameState();
+
+	Controller* const getController(const uint32_t i_cuEntityId);
 
 protected:
+	std::unordered_map<uint32_t, Controller*> m_umControllers;
+
 private:
 };
