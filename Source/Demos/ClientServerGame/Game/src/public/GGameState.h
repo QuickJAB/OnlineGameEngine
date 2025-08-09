424435f04c11ff82d7c4a95df1a9a7d2820240db
QuickJAB <Redman9782@gmail.com>
2025-08-10 00:34:43 +0100

    Initial setup for projectile controller

--- Source/Demos/ClientServerGame/Game/src/public/GGameState.h
+++ Source/Demos/ClientServerGame/Game/src/public/GGameState.h
@@ -1,13 +1,18 @@
 #pragma once
 
+#include <vector>
+
 #include <jNet/GameState.h>
 
 struct GameStateUpdatePkt;
+class GProjectileController;
 
 class GGameState : public GameState
 {
 public:
 protected:
+	std::vector<GProjectileController*> m_vProjectiles;
+
 private:
 
 public:
